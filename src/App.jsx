c3477bc01030642b766a600f6f9058317326dd03
kimgahyoun <hyeon3821@gmail.com>
2026-02-15 22:59:05 +0900

    chore:리액트 라우터 돔 세팅(#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Meds from './pages/Meds';
 
 function App() {
-  return;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/medicines" element={<Meds />} />
+    </Routes>
+  );
 }
 
 export default App;
