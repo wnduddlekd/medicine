423a837b836352425b58803ff0769e06013dca31
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2026-03-09 12:56:26 +0900

    [FEAT] 메인 레이아웃 구현 (#14)
    * docs: 템플릿에 closes 추가
    
    * feat: 메인 레이아웃 구현(#12)
    
    * fix: import 순서에 따른 에러 수정
    
    * refactor: 레이아웃 기반 라우팅 구조로 변경 (#12)

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
 import './styles/theme.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
