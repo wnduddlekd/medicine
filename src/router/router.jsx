6431e48c602398ea7fc66e6fc1c4a0595af8d1ee
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2026-03-27 20:26:11 +0900

    [Feat] 테스트 허브 제작 (#18)
    * chore: 작업 환경 변경으로 package-lock 갱신
    
    * feat: 테스트 페이지들 라우터 연결(#17)
    
    * feat: 테스트 허브 구현(#17)
    
    * refactor: 메인 레이아웃 수정(#17)
    
    * refactor: ThemeTest에서 존재하지 않는 호버 컬러박스 삭제
    
    ---------
    
    Co-authored-by: wnduddlekd <ju03209@naveer.com>

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,16 +1,24 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import Calender from '../pages/Calender';
 import CheckIn from '../pages/CheckIn';
 import Meds from '../pages/Meds';
 import Insights from '../pages/Insights';
 import MainLayout from '../Layout/MainLayout';
+import TestHub from '../tests/TestHub';
+import ThemeTest from '../tests/ThemeTest';
+import ButtonTest from '../tests/ButtonTest';
 
 export const Router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
-      { path: '/', element: <Home /> },
+      { path: '/', element: <Navigate to="/test" /> },
+      { path: '/test', element: <TestHub /> },
+      { path: '/test/ThemeTest', element: <ThemeTest /> },
+      { path: '/test/ButtonTest', element: <ButtonTest /> },
+
+      { path: '/home', element: <Home /> },
       { path: '/check-in', element: <CheckIn /> },
       { path: '/my-meds', element: <Meds /> },
       { path: '/calender', element: <Calender /> },
