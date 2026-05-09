ebf6d408b227add99591b0959ffdbae3048d4ed7
wnduddlekd <ju03209@naveer.com>
2026-05-09 16:07:12 +0900

    feat: 메인 레이아웃에 헤더 추가 및 레이아웃 너비 지정

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/commons/Header';
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-base-100 flex flex-col items-center">
-      <div className="w-full flex flex-col flex-1 items-center p-5">
+      <div className="w-full flex flex-col flex-1 gap-4 items-center p-5">
         {/* Header */}
+        <Header />
 
         {/* Page */}
-        <main className="flex-1">
+        <main className="flex-1 max-w-205 w-full">
           <Outlet />
         </main>
       </div>
