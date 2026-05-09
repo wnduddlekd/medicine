8cb7cc5e48355352d41194b217c5ba49764a5ecf
wnduddlekd <ju03209@naveer.com>
2026-05-09 18:26:12 +0900

    feat: 캘린더 레이아웃 추가(#19)

---
+++ src/components/Home/HomeCalender.jsx
@@ -0,0 +1,7 @@
+export default function HomeCalender() {
+  return (
+    <div className="min-w-120 w-full h-95 card-base">
+      <div>캘린더 입니당</div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import DailyDoseFeedback from '../components/Home/DailyDoseFeedback';
+import HomeCalender from '../components/Home/HomeCalender.jsx';
 import { medicines } from '../Mocks/medicationMock.js';
 
 export default function Main() {
   return (
     <div className="space-y-5">
       <DailyDoseFeedback medicines={medicines} />
+      <div className="flex">
+        <div>
+          <HomeCalender />
+        </div>
+        <div className="flex flex-col items-center w-full"></div>
+      </div>
     </div>
   );
 }
