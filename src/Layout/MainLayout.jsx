a131217540a561ced81297933c17ad9242153700
wnduddlekd <ju03209@naveer.com>
2026-05-09 14:43:58 +0900

    feat: 메인 레이아웃 패딩값 추가(#19)

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-base-100 flex flex-col items-center">
-      <div className="w-full flex flex-col flex-1 items-center">
+      <div className="w-full flex flex-col flex-1 items-center p-5">
         {/* Header */}
 
         {/* Page */}
