5249f470c8051974a718d2dbeeee715ea53214ab
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2026-05-24 19:11:34 +0900

    [FEAT] 메인 반응형 구현 (#29)
    * docs: 템플릿에 closes 추가
    
    * feat: 메인 페이지 반응형 레이아웃 구현(#28)
    
    * fix: 사용하지 않는 import 제거(#28)

--- src/components/Home/HomeCalender.jsx
+++ src/components/Home/HomeCalender.jsx
@@ -1,6 +1,6 @@
 export default function HomeCalender() {
   return (
-    <div className="min-w-120 w-full h-95 card-base">
+    <div className="min-w-100 w-full h-95 card-base">
       <div>캘린더 입니당</div>
     </div>
   );
