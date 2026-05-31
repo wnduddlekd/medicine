7c0cbe4e2962d50a9c506e0fdfbff970f6a19d3e
wndud <ju03209@naver.com>
2026-05-31 18:43:19 +0900

    feat: todo 목데이터 추가(#31)

---
+++ src/Mocks/todoMock.js
@@ -0,0 +1,17 @@
+export const todoData = [
+  {
+    id: 1,
+    text: '24일 ㅇㅇ병원 가기',
+    completed: false,
+  },
+  {
+    id: 2,
+    text: '약 복용하기',
+    completed: false,
+  },
+  {
+    id: 3,
+    text: '산책하기',
+    completed: true,
+  },
+];
